859aeb2bef89afdf565a9e461b62293c6bc8ac8b
pdeepak936 <67180496+pdeepak936@users.noreply.github.com>
2024-03-19 19:23:37 +0530

    updode

---
+++ index.js
@@ -0,0 +1,16 @@
+function clickFunction(){
+    var randomNumber1 = Math.floor(Math.random() * 6) +1;
+    var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+    var randomNumber2 = Math.floor(Math.random() * 6) +1;
+    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+    
+    if(randomNumber1>randomNumber2){
+        document.querySelectorAll("h1")[0].innerHTML = "Player 1 Wins";
+    }else if(randomNumber1 == randomNumber2){
+        document.querySelectorAll("h1")[0].innerHTML = "Both Player Wins";
+    }else{
+        document.querySelectorAll("h1")[0].innerHTML = "Player 2 Wins";
+    }
+}
